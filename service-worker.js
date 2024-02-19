caf58779ca08a8efc10e35ec82e0ed34c4914c55
Rohit <mailrohit@webgrity.net>
2024-02-19 14:14:01 +0530

    main commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'simple-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
